c1150569d5951f7c5f23675345487fe76a141336
rosemutai <chepngetichrose2030@gmail.com>
2023-01-19 17:38:06 +0300

    app

---
+++ zoezifitness/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div className="App w-full pb16 font-roboto bg-black/90 min-h-screen">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
