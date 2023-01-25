d2c266f28752c8819c1be263f68c8e1e783ed3af
rosemutai <chepngetichrose2030@gmail.com>
2023-01-25 16:25:30 +0300

    mobile responsive subscription section

--- zoezifitness/src/components/Subscribe.tsx
+++ zoezifitness/src/components/Subscribe.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import mail from '../images/maill.jpg'
+import mail from '../images/mesage.png'
 
 const Subscribe = () => {
   return (
-     <div className='md:w-7/12 md:h-80  md:mx-auto md:mt-14 mb-5 bgyellow-600 md:flex md:justify-center rounded-t-md'>
-      <div className="image  md:w1/2 relative ">
-        <FontAwesomeIcon icon={faArrowDown} className='text-3xl absolute -top-1 left-16 mt-4 w-16 h-16 text-white ' />
-        <FontAwesomeIcon icon={faEnvelope} className='text-3xl w-24 h-24 text-white p-14  rounded-full bg-yellow-600' />
-        
+     <div className='w-11/12 md:w-7/12 h-80  mx-auto mt-2 mb-12 flex justify-between md:justify-center rounded-t-md'>
+      <div  className="imagee w-1/2">
+        <img src={mail} className='mt-16 -ml-2 md:ml-0 md:mt-0' alt="email notification" />
       </div>
-      <div className="left md:w1/2 md:ml-7  bglack rounded-r-md ">
-        <h5 className='md:text-sm mt-10 text-yellow-600 upper'>subscribe now</h5>
-        <h4 className='md:text-2xl text-greyish my-2 font-medium'>Get every single update</h4>
-        <form className='mt-4'>
-          <input type="email" name="email" className='bg-black/90 border border-greyish py-1 px-3
-            placeholde:text-white' placeholder='Enter your email'/>
-          <span><button className='text-white py-1.5 px-3 bg-yellow-600 text-sm'>subscribe now</button></span>
+      <div className="rightt w-1/2 flex flex-col justify-center">
+        <h5 className='text-xs md:text-sm text-yellow-600 upper'>subscribe now</h5>
+        <h4 className='text-sm md:text-xl text-greyish/80 my-2 font-medium'>Get every single update</h4>
+        <form className='mt-1 md:mt-4 flex flex-col md:flex-row'>
+          <input type="email" name="email" className='bg-black/90 border border-greyish py-1 px-3 w-full md:w-9/12
+            placeholde:text-white placeholder:text-xs rounded-md' placeholder='Enter your email'/>
+          <button className='text-white p-1  bg-yellow-600 text-sm md:ml-2 rounded-md
+            border-2 border-transparent hover:bg-black/90 hover:border-yellow-600 mt-2 md:mt-0'>subscribe</button>
         </form>
         
       </div>
