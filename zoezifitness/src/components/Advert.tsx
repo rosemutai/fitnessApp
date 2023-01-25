17f90cbe8e52dd8a21f359f241c17bf7a986d6c8
rosemutai <chepngetichrose2030@gmail.com>
2023-01-25 16:10:35 +0300

    mobile responsive advert section

--- zoezifitness/src/components/Advert.tsx
+++ zoezifitness/src/components/Advert.tsx
@@ -20,26 +20,26 @@ const array: AdType[] =[
 
 const Advert = () => {
   return (
-    <div className='md:w-11/12 mx-auto md:flex md:justify-center mt-16 bg-black -600 b-10'>
-      <div className="ad  md:flex md:flex-col md:justify-center md:items-center my-6 mr-9">
+    <div className='w-11/12 mx-auto flex flex-wrap justify-center mt-24 bg-black b-10'>
+      <div className="ad  flex flex-col justify-center items-center my-3 md:my-6 mr-3 md:mr-9">
         <img src={image2} className='w-32 h-32' alt="ad" />
         <h5 className='total font-extrabold text-xl text-white mt-2'>700+</h5>
         <h6 className='ad-title font-medium text-xs text-yellow-600 white my-1 uppercase'>trainess</h6>
       </div>
 
-      <div className="ad  md:flex md:flex-col md:justify-center md:items-center my-6 mr-9">
+      <div className="ad  flex flex-col justify-center items-center my-3 md:my-6 mr-3 md:mr-9">
         <img src={image2} className='w-32 h-32' alt="ad" />
         <h5 className='total font-bold text-xl text-white  mt-2'>100+</h5>
         <h6 className='ad-title font-medium text-xs text-yellow-600 my-1 uppercase'>workouts</h6>
       </div>
 
-      <div className="ad  md:flex md:flex-col md:justify-center md:items-center my-6 mr-9">
+      <div className="ad flex flex-col justify-center items-center my-3 md:my-6 mr-3 md:mr-9">
         <img src={image2} className='w-32 h-32' alt="ad" />
         <h5 className='total font-extrabold text-xl text-white mt-2'>60</h5>
         <h6 className='ad-title font-medium text-xs text-yellow-600 my-1 uppercase'>instructors</h6>
       </div>
 
-      <div className="ad  md:flex md:flex-col md:justify-center md:items-center my-6 mr-9">
+      <div className="ad  md:flex md:flex-col md:justify-center md:items-center my-3 md:my-6 mr-3 md:mr-9">
         <img src={image2} className='w-32 h-32' alt="ad" />
         <h5 className='total font-extrabold text-xl text-white mt-2'>23</h5>
         <h6 className='ad-title font-medium text-xs text-yellow-600 my-1 uppercase'>counties</h6>
