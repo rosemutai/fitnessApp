4e2bafe1a807cc0e2169481430e2898950feee04
rosemutai <chepngetichrose2030@gmail.com>
2023-01-26 15:16:51 +0300

    configure react router

--- zoezifitness/src/index.tsx
+++ zoezifitness/src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Contact from './routes/Contact';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'contact/',
+    element: <Contact />
+  }
+])
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider  router={router} />
 );
 
