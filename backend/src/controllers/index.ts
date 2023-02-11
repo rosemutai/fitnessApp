b6a92d7d77eebb4918fa35e02edc28ea50e10cc9
rosemutai <chepngetichrose2030@gmail.com>
2023-02-11 14:58:49 +0300

    Add create Contact controller

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+const Contact = require('../models/contact')
+
+const createContact = async(req: Request, res: Response) => {
+  const contact = req.body
+
+  const contactToSave = new Contact(contact)
+
+  try {
+    const contact = await Contact.save(contactToSave)
+    return res.status(201).json(contact)
+  } catch (error) {
+     res.status(409).json({message: error.message}) 
+  }
+  
+  await contactToSave.save()
+
+
+
+}
+
+module.exports = { createContact}
