908477e07615b3a52f1bb22a4c63e29e438e5044
rosemutai <chepngetichrose2030@gmail.com>
2023-02-12 18:25:22 +0300

    configure mongodb

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+const mongoose = require('mongoose')
+const contactRouter = require("./routes/index");
+require('dotenv').config()
+
+const app = express()
+app.use('/api', contactRouter)
+
+const MONGOURL = process.env.MONGOURL
+const PORT = process.env.PORT
+
+mongoose.connect(MONGOURL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+  })
+  .catch((error:unknown) => console.log(error))
+
+// password: wsqchBmUkFdnmbmB
+
+// mongoURL: mongodb+srv://admin:<password>@cluster0.veest8g.mongodb.net/?retryWrites=true&w=majority
