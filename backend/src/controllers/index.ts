5f0dad86f45c98791b876b9658aafb0f40d56463
rosemutai <chepngetichrose2030@gmail.com>
2023-03-03 14:16:17 +0300

    Add mailchimp email subscription

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express"
+const https = require('https')
 const Contact = require('../models/contact')
+require('dotenv').config()
+const LISTID = process.env.LISTID
 
 const createContact = async(req: Request, res: Response) => {
   const contact = req.body
@@ -15,4 +18,35 @@ const createContact = async(req: Request, res: Response) => {
 
 }
 
-module.exports = { createContact}
+const subscribeToNewsletter = (req: Request, res: Response) => {
+
+  const {email} = req.body
+
+  let data = {
+    members: [{
+      email_address: email,
+      status: 'subscribed',
+    }]
+  }
+
+  const jsonData = JSON.stringify(data)
+  const url = `https://us14.api.mailchimp.com/3.0/lists/${LISTID}`
+  const options = {
+    method: "POST",
+    auth: process.env.MAILCHIMPAPI
+  }
+
+  const request = https.request(url, options, (response: Response) => {
+    if(response.statusCode === 200) {
+      return res.status(201).json({ msg: "You have successfully subscribed to our email"})
+    }
+    else {
+      return res.status(409).json({ msg: "Failed to subscribe to our email, please try again later"})
+    }
+  })
+  request.write(jsonData)
+  request.end()
+
+}
+
+module.exports = { createContact, subscribeToNewsletter}
