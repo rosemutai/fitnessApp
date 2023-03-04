393463c646e4d83f336d66f6131f95eda9d29c8c
rosemutai <chepngetichrose2030@gmail.com>
2023-03-04 13:28:42 +0300

    Add sendgrid email subscription

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -10,8 +10,9 @@ sgMail.setApiKey(process.env.SENDGRIDAPI)
 sgClient.setApiKey(process.env.SENDGRIDAPI)
 
 type contactType = {
+  email: string
   conf_num: number,
-  custom_fields: {}
+  custom_fields: {customm_field: number}
 }
 
 const createContact = async(req: Request, res: Response) => {
@@ -28,58 +29,6 @@ const createContact = async(req: Request, res: Response) => {
 
 }
 
-// const subscribeToNewsletter = (req: Request, res: Response) => {
-
-//   const {email} = req.body
-
-//   let data = {
-//     members: [{
-//       email_address: email,
-//       status: 'subscribed',
-//     }]
-//   }
-
-//   const jsonData = JSON.stringify(data)
-//   const url = `https://us14.api.mailchimp.com/3.0/lists/${LISTID}`
-//   const options = {
-//     method: "POST",
-//     auth: process.env.MAILCHIMPAPI
-//   }
-
-//   const request = https.request(url, options, (response: Response) => {
-//     if(response.statusCode === 200) {
-//       return res.status(201).json({ msg: "You have successfully subscribed to our email"})
-//     }
-//     else {
-//       return res.status(409).json({ msg: "Failed to subscribe to our email, please try again later"})
-//     }
-//   })
-//   request.write(jsonData)
-//   request.end()
-
-// }
-
-function randNum() {
- return Math.floor(Math.random() * 90000) + 10000;
-}
-
-async function addContact( email:string, confNum:number) {
- const customFieldID = await getCustomFieldID('conf_num');
-
-  const data = {
-   "contacts": [{
-     "email": email,
-     "custom_fields": {}
-   }]
-  };
- data.contacts[0].custom_fields[customFieldID] = confNum;
- const request = {
-   url: `/v3/marketing/contacts`,
-   method: 'PUT',
-   body: data
- }
- return sgClient.request(request);
-}
 
 async function getCustomFieldID(customFieldName:string) {
  const request = {
@@ -93,25 +42,75 @@ async function getCustomFieldID(customFieldName:string) {
 
 const subscribeToNewsletter = async (req: Request, res: Response) => {
 
-  const confirmationNum :number = randNum()
+  const confirmationNum :number = Math.floor(Math.random() * 90000) + 10000;
   const params = new URLSearchParams({
     conf_num: confirmationNum.toString(),
     email: req.body.email,
   })
 
-  const confirmationURL = req.protocol + '://' + req.headers.host + '/confirm/?' + params
+  const confirmationURL = `${req.protocol}://${req.headers.host}/confirm/?${params}`
   const msg = {
     to: req.body.email,
-    from: 'rchepngetich0@gmail.com',
+    from: process.env.SENDEREMAIL,
     subject: `Confirm your subscription to our newsletter`,
     html: `Hello,<br>Thank you for subscribing to our newsletter. 
       Please complete and confirm your subscription by <a href="${confirmationURL}"> clicking here</a>.`
   }
-    await addConntact(req.body.email, confirmationNum)
+
+  try {
+    await addContact(req.body.email, confirmationNum)
     await sgMail.send(msg)
-    res.render('message', 
-    { message: 'Thank you for signing up for our newsletter! Please complete the process by confirming the subscription in your email inbox.' });
+    res.status(200).send('Thank you for subscribing! Please check your email to confirm your subscription.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error subscribing. Please try again later.');
+  }
 
 }
+
+// confirmation link
+const confirmEmail = async (req: Request, res: Response) => {
+  const email = req.query.email?.toString()
+  const conf_num = parseInt(req.query.conf_num?.toString())
+  try {
+    // Update subscriber status to confirmed in SendGrid marketing contact list
+    await confirmContact(email, confNum);
+    // Send welcome email to subscriber
+    const welcomeMsg = {
+      to: email,
+      from: process.env.SENDEREMAIL,
+      subject: 'Welcome to our newsletter!',
+      html: `Hello,<br>Thank you for subscribing to our newsletter. We'll send you updates and news about our company regularly. Stay tuned!`,
+    };
+    await sgMail.send(welcomeMsg)
+    res.status(200).send('Thank you for confirming your subscription. You will receive a welcome email shortly.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error confirming subscription. Please try again later.');
+    
+  }
+}
+
+// Add subscriber to SendGrid marketing contact list
+async function addContact(email: string, confNum: number) {
+  const customFieldID = await getCustomFieldID('conf_num');
+  const data: any = {
+    contacts: [
+      {
+        email: email,
+        custom_fields: {},
+      },
+    ],
+  };
+  data.contacts[0].custom_fields[customFieldID] = confNum;
+  const request = {
+    url: '/v3/marketing/contacts',
+    method: 'PUT',
+    body: data,
+  };
+  await sgClient.request(request);
+}
+
+
 
 module.exports = { createContact, subscribeToNewsletter}
